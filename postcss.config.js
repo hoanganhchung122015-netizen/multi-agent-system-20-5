16f42a87b713d8f8c3c062bbacf5949c1b56c741
hoanganhchung122015-netizen <hoanganhchung122015@gmail.com>
2025-12-29 22:49:18 +0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
   },
-};
+  plugins: [],
+}
